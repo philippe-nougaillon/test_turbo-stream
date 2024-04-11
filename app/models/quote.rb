88f9c16d4c69f99f0bccf122846c37caef1e1046
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-04-11 22:10:53 +0200

    turbo_stream activated

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,8 @@
 class Quote < ApplicationRecord
+
+    after_create_commit -> { broadcast_prepend_to "quotes", 
+                                                    partial: "quotes/quote", 
+                                                    locals: { quote: self }, 
+                                                    target: "quotes" }
+
 end
